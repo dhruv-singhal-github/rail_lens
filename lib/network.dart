429ea3b675ebd1bb4c58c40eb1e2b198d21d32e2
GedduWarkh <gedduWarkh@users.noreply.github.com>
2019-06-17 12:45:56 +0530

    Change network services

--- lib/network.dart
+++ lib/network.dart
@@ -8,12 +8,13 @@ import 'models/model.dart';
 class RailApi {
   final Client _client = Client();
   static const String _url =
-      "http://172.16.21.96:8080/stationimages/rest/stationimages/";
+      "http://samvad.cris.org.in/stationimages/rest/stationimages/";
   static const String _login = 'login';
-  static const String _changePassword = 'changePassword';
-  static const String _upload = 'uploadPics';
-  static const String _download = 'downloadThumbnail';
+  static const String _changePassword = 'changepassword';
+  static const String _upload = 'uploadimage';
+  static const String _downloadImageList = 'stnimages';
 
+  static const Duration _timeOutWaitMilliSeconds = Duration(milliseconds: 2000);
   static final RailApi _railApi = RailApi._internal();
 
   factory RailApi() {
@@ -22,36 +23,54 @@ class RailApi {
 
   RailApi._internal();
 
+  //TODO: Beware of string interpolation
   Future<AuthorizationModel> login(String username, String password) async {
+    print('Login Basic: ${base64.encode(utf8.encode('$username:$password'))}');
     AuthorizationModel authorizationModel = await _client
         .post(Uri.parse(_url + _login),
-            headers: {
-              'accept': 'application/json',
-              'Content-type': 'application/x-www-form-urlencoded'
-            },
-            //TODO: Fix possible security error
-            body: 'name=' + username + '&password=' + password)
-        .timeout(Duration(seconds: 10))
+        headers: {
+          'accept': 'application/json',
+          'Content-type': 'application/x-www-form-urlencoded',
+          'Authorization':
+          'Basic ${base64.encode(utf8.encode('$username:$password'))}',
+        },
+        //TODO: Fix possible security error
+        body: 'name=' + username + '&password=' + password)
+        .timeout(_timeOutWaitMilliSeconds)
         .then((response) => response.body)
         .then((body) {
-          print('RESPONSE BODY ALERT! + $body');
-          return body;
-        })
-        .then((val) {
-          if (val == 'true') {
-            return '"authorization": "true", "isDefault": "false", "locations": "[DEL, STD]"';
-          } else {
-            return '"authorization": "false", "isDefault": "false", "locations": "[DEL, STD]"';
-          }
-        })
+      print('RESPONSE BODY ALERT! $body');
+      return body;
+    })
         .then(json.decode)
         .then((json) => AuthorizationModel.fromJson(json));
     return authorizationModel;
   }
 
-  Future<bool> changePassword(
+  Future<AuthorizationModel> changePassword(
       String username, String oldPassword, String newPassword) async {
-    return Future<bool>(() => true);
+    print(
+        'Change pass Basic: ${base64.encode(utf8.encode('$username:$oldPassword'))}');
+    AuthorizationModel authorizationModel = await _client
+        .post(Uri.parse(_url + _changePassword),
+        headers: {
+          'accept': 'application/json',
+          'Content-type': 'application/x-www-form-urlencoded',
+          'Authorization':
+          'Basic ${base64.encode(utf8.encode('$username:$oldPassword'))}',
+        },
+        //TODO: Fix possible security error
+        body:
+        'name=$username&password=$oldPassword&newpassword=$newPassword')
+        .timeout(_timeOutWaitMilliSeconds)
+        .then((response) => response.body)
+        .then((body) {
+      print('RESPONSE BODY ALERT! $body');
+      return body;
+    })
+        .then(json.decode)
+        .then((json) => AuthorizationModel.fromJson(json));
+    return authorizationModel;
   }
 
   void _basicAuthLowLevelImpl() {
@@ -67,24 +86,36 @@ class RailApi {
     var future = client
         .send(request)
         .then((response) => response.stream
-            .bytesToString()
-            .then((value) => print(value.toString())))
+        .bytesToString()
+        .then((value) => print(value.toString())))
         .catchError((error) => print(error.toString()));
   }
 
-  Future<List<ThumbnailDataModel>> fetchImageList(
-      int stationCode, Credentials cr) async {
-    return Future<List<ThumbnailDataModel>>(() => [
-          ThumbnailDataModel(DateTime.now(), 1, "Image here"),
-          ThumbnailDataModel(DateTime.now(), 2, "Image here"),
-          ThumbnailDataModel(DateTime.now(), 3, "Image here"),
-          ThumbnailDataModel(DateTime.now(), 4, "Image here"),
-          ThumbnailDataModel(DateTime.now(), 5, "Image here"),
-          ThumbnailDataModel(DateTime.now(), 6, "Image here"),
-        ]);
+  Future<ImageList> fetchImageList(
+      String locid, String subinitid, Credentials creds) async {
+    print('Fetch Image list Basic:' +
+        '${base64.encode(utf8.encode('${creds.username}:${creds.password}'))}');
+    ImageList imageList = await _client
+        .get(Uri.parse(_url + _downloadImageList+'/$locid/$subinitid'),
+      headers: {
+        'accept': 'application/json',
+        'Content-type': 'application/x-www-form-urlencoded',
+        'Authorization':
+        'Basic ${base64.encode(utf8.encode('${creds.username}:${creds.password}'))}',
+      },)
+        .timeout(_timeOutWaitMilliSeconds)
+        .then((response) => response.body)
+        .then((body) {
+      print('RESPONSE BODY ALERT! $body');
+      return body;
+    })
+        .then(json.decode)
+        .then((json) => ImageList.fromJson(json));
+    return imageList;
   }
 
   Future<bool> uploadImage(int stationCode, int imageId, Credentials cr) async {
     return Future<bool>(() => true);
   }
 }
+	
