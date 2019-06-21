1db0e3ed10b3fac93b7151085fe75e64fd0e6b42
GedduWarkh <gedduWarkh@users.noreply.github.com>
2019-06-21 12:18:03 +0530

    Bug fix

--- lib/reusable_ui.dart
+++ lib/reusable_ui.dart
@@ -36,29 +36,71 @@ class LoadingCircular extends StatelessWidget {
   }
 }
 
-class ChangeImageConfirmation extends StatelessWidget{
+class ChangeImageConfirmation extends StatefulWidget {
   final File prevImage;
   final File newImage;
 
-  const ChangeImageConfirmation({Key key, this.prevImage, this.newImage}) : super(key: key);
+  const ChangeImageConfirmation({Key key, this.prevImage, this.newImage})
+      : super(key: key);
+
+  @override
+  ChangeImageState createState() {
+    return ChangeImageState();
+  }
+}
+
+class ChangeImageState extends State<ChangeImageConfirmation> {
+  DateTime _selectedDate = DateTime.now();
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: _selectedDate,
+        firstDate: DateTime(2017),
+        lastDate: DateTime.now());
+    if (picked != null && picked != _selectedDate)
+      setState(() {
+        _selectedDate = picked;
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Image.file(prevImage),
-          SizedBox(
-            width: 100,
-            height: 100,
-            child: Icon(Icons.arrow_forward),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.file(widget.prevImage),
+              SizedBox(
+                width: 100,
+                height: 100,
+                child: Icon(Icons.arrow_forward),
+              ),
+              Column(
+                children: <Widget>[
+                  Image.file(widget.newImage),
+                  RaisedButton(
+                    onPressed: () => _selectDate(context),
+                    child: Text("${_selectedDate.toLocal()}"),
+                  ),
+                ],
+              ),
+            ],
           ),
-          Image.file(newImage),
+          RaisedButton(
+            onPressed: ()=>_confirmUpload(),
+            child: Text('Confirm'),
+          )
         ],
       ),
     );
   }
 
+  void _confirmUpload() {
 
+  }
 }
